f00a323926cd3f24f9184e0416b481f69ddca911
Damian Krzeminski <pirxpilot@code42day.com>
2016-08-31 09:35:18 -0400

    add fetching customer and cases

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/dumpdesk');
+var dumpdesk = require('./lib/dumpdesk');
+
+
+var argv = require('yargs')
+  .demand('user')
+  .demand('password')
+  .demand('host')
+  .default({
+    database: 'mongodb://localhost:27017/desk_com',
+  })
+  .argv;
+
+
+argv.db = require('mniam').db(argv.database);
+
+dumpdesk(argv);
--- test/dumpdesk.js
+++ test/dumpdesk.js
@@ -1,9 +1,7 @@
 var should = require('should');
-var dumpdesk = require('../');
 
 describe('dumpdesk node module', function () {
   it('must have at least one test', function () {
-    dumpdesk();
     should.fail('Need to write tests.');
   });
 });
