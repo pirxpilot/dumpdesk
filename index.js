b0baf335e0a673079821e450ff64918886c7ce8f
Damian Krzeminski <pirxpilot@code42day.com>
2016-08-31 10:03:54 -0400

    fetch embedded case content

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var dumpdesk = require('./lib/dumpdesk');
 
-
 var argv = require('yargs')
   .demand('user')
   .demand('password')
   .demand('host')
   .default({
-    database: 'mongodb://localhost:27017/desk_com',
+    database: 'mongodb://localhost:27017/desk_com'
   })
   .argv;
 
-
 argv.db = require('mniam').db(argv.database);
 
-dumpdesk(argv);
+dumpdesk(argv, function(err) {
+  if (err) {
+    console.err('Error: %s', err);
+  }
+});
